31bea60232e7e4299646b5352d0ebe45d46268bb
gowthami <allamgowthamicse@gmail.com>
2021-06-04 12:15:56 +0530

    firstcommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServiceService } from './service.service';
+import { urlServices } from './serviceUrls';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ViewComponent } from './view/view.component';
+import { ChartsComponent } from './charts/charts.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ViewComponent,
+    ChartsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,HttpClientModule, FormsModule, ReactiveFormsModule
+  ],
+ 
+  bootstrap: [AppComponent],
+  providers: [ServiceService,urlServices]
+})
+export class AppModule { }
